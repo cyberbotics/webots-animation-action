de9811d2c88310e097b70553fe32d4d04562bf87
Darko Lukic <lukicdarkoo@gmail.com>
2020-09-25 13:02:31 +0200

    Quote name

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ python3 /bin/scripts/run.py
 NAME=$(curl https://api.github.com/users/${GITHUB_ACTOR} | jq -r .name)
 ID=$(curl https://api.github.com/users/${GITHUB_ACTOR} | jq -r .id)
 
-git config --global user.name ${NAME}	
-git config --global user.email ${ID}+${GITHUB_ACTOR}@users.noreply.github.com
+git config --global user.name "${NAME}"
+git config --global user.email "${ID}+${GITHUB_ACTOR}@users.noreply.github.com"
 git reset --hard
 git fetch
 git checkout gh-pages || git checkout -b gh-pages

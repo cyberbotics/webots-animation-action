7558043d9dbe060cbb087923b8b7976fd2f99e86
Darko Lukic <lukicdarkoo@gmail.com>
2020-10-07 09:29:57 +0200

    Allow dependencies

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-mkdir /tmp/animation
-python3 /bin/scripts/run.py
+# Install dependencies
+python3 /bin/scripts/execute_init.py
 
+# Generate animation
+mkdir /tmp/animation
+python3 /bin/scripts/generate_animation.py
+
+# Publish animation to `gh-pages` branch
 NAME=$(curl https://api.github.com/users/${GITHUB_ACTOR} | jq -r .name)
 ID=$(curl https://api.github.com/users/${GITHUB_ACTOR} | jq -r .id)
-
 git config --global user.name "${NAME}"
 git config --global user.email "${ID}+${GITHUB_ACTOR}@users.noreply.github.com"
 git reset --hard

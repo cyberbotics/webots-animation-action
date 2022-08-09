3da4587ca71c80fa5a3c9bec13d3c1413f72b64a
ThomasOliverKimble <tokimble@gmail.com>
2022-08-09 15:06:32 +0200

    add safe directory

--- wb_animation_action/utils/git.py
+++ wb_animation_action/utils/git.py
@@ -58,6 +58,7 @@ def push_directory_to_branch(source_directory, destination_directory='.', destin
 
     _init()
 
+    subprocess.check_output(['git', 'config', '--global', '--add', 'safe.directory', '/github/workspace'])
     subprocess.check_output(['git', 'reset', '--hard'])
     subprocess.check_output(f'git checkout {destination_branch} || git checkout -b {destination_branch}', shell=True)
 
